8e9797b01e97d2646ac8e58807a4800e8640f8b0
JamesRBrown <generaldelta@yahoo.com>
2013-10-29 11:34:34 -0600

    Update jquery.search.js

--- jquery.search.js
+++ jquery.search.js
@@ -1,8 +1,8 @@
 /*
 	Title: 			jquery.search.js
 	Author: 		James R. Brown
-	Date: 			6/7/13
-	Version: 		2.0
+	Date: 			10/1/13
+	Version: 		4.0
 	Requirements: 	jQuery 1.1.4+ 
 	
 	The MIT License (MIT)
@@ -25,10 +25,35 @@
 	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
 	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
+//supporting jQuery selectors
 jQuery.expr[':'].iContains = function(a,i,m){
      return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase())>=0;
 };
+jQuery.expr[':'].valueContains = function(a,i,m){
+     var matches = 0; 
+    jQuery(a).find('[value]').filter('input,option:selected').each(function(){
+        if(jQuery(this).attr('value').indexOf(m[3])>=0) matches++;
+        if(jQuery(this).text().indexOf(m[3])>=0) matches++;
+    });
+    return matches > 0;
+};
+jQuery.expr[':'].iValueContains = function(a,i,m){
+    var matches = 0; 
+    jQuery(a).find('[value]').filter('input,option:selected').each(function(){
+        if(jQuery(this).attr('value').toUpperCase().indexOf(m[3].toUpperCase())>=0) matches++;
+        if(jQuery(this).text().toUpperCase().indexOf(m[3].toUpperCase())>=0) matches++;
+    });
+    return matches > 0;
+};
+jQuery.expr[':'].wildcard = function(a,i,m){
+    var regex  = "^"+m[3].replace(/\?/g,'.?').replace(/\*/g,'.*?')+"jQuery";
+    var re = new RegExp(regex, 'i');
+    var text = jQuery(a).text();
+    return text.match(re) ? true : false;
+};
 
+//Text searches
 jQuery.extend( jQuery.fn, {
     andSearch: function(p) {
         if (p.match(/[^\s]+/g) === null) return this;        
@@ -37,7 +62,7 @@ jQuery.extend( jQuery.fn, {
 		for (var i = 0; i < t.length; i++) {
 			c += ':contains(' + t[i] + ')';
 		}
-		return $(this).filter(c);
+		return jQuery(this).filter(c);
     },
     orSearch: function(p) {
         if (p.match(/[^\s]+/g) === null) return this;        
@@ -47,12 +72,12 @@ jQuery.extend( jQuery.fn, {
 			c += ':contains(' + t[i] + ')';
 			if(i+1 !== t.length) c += ',';
 		}
-		return $(this).filter(c);
+		return jQuery(this).filter(c);
     },
-	search: function(p) {
-	   if (p.match(/.+/g) === null) return this;
-		return $(this).filter(':contains(' + p + ')');
-	},
+    search: function(p) {
+       if (p.match(/.+/g) === null) return this;
+            return jQuery(this).filter(':contains(' + p + ')');
+    },
     iAndSearch: function(p) {
         if (p.match(/[^\s]+/g) === null) return this;        
         var t = p.match(/[^\s]+/g);
@@ -60,7 +85,7 @@ jQuery.extend( jQuery.fn, {
 		for (var i = 0; i < t.length; i++) {
 			c += ':iContains(' + t[i] + ')';
 		}
-		return $(this).filter(c);
+		return jQuery(this).filter(c);
     },
     iOrSearch: function(p) {
         if (p.match(/[^\s]+/g) === null) return this;        
@@ -68,12 +93,93 @@ jQuery.extend( jQuery.fn, {
 		var c = '';
 		for (var i = 0; i < t.length; i++) {
 			c += ':iContains(' + t[i] + ')';
-			if(i+1 !== t.length) c += ',';
+			if(i+1 !== t.length){
+                c += ',';
+            }
 		}
-		return $(this).filter(c);
+		return jQuery(this).filter(c);
     },
-	iSearch: function(p) {
+    iSearch: function(p) {
 	   if (p.match(/.+/g) === null) return this;
-		return $(this).filter(':iContains(' + p + ')');
+		return jQuery(this).filter(':iContains(' + p + ')');
 	}
+});
+
+//Value Searches
+jQuery.extend( jQuery.fn, {
+    andValueSearch: function(p) {
+        if (p.match(/[^\s]+/g) === null) return this;        
+        var t = p.match(/[^\s]+/g);
+		var v = '';
+		for (var i = 0; i < t.length; i++) {
+			v += ':valueContains(' + t[i] + ')';
+		}
+		return jQuery(this).filter(v);
+    },
+    orValueSearch: function(p) {
+        if (p.match(/[^\s]+/g) === null) return this;        
+        var t = p.match(/[^\s]+/g);
+		var v = '';
+		for (var i = 0; i < t.length; i++) {
+			v += ':valueContains(' + t[i] + ')';
+			if(i+1 !== t.length) v += ',';
+		}
+		return jQuery(this).filter(v);
+    },
+    valueSearch: function(p) {
+       if (p.match(/.+/g) === null) return this;
+            return jQuery(this).filter(':valueContains(' + p + ')');
+    },
+    iAndValueSearch: function(p) {
+        if (p.match(/[^\s]+/g) === null) return this;        
+        var t = p.match(/[^\s]+/g);
+		var v = '';
+		for (var i = 0; i < t.length; i++) {
+			v += ':iValueContains(' + t[i] + ')';
+		}
+		return jQuery(this).filter(v);
+    },
+    iOrValueSearch: function(p) {
+        if (p.match(/[^\s]+/g) === null) return this;        
+        var t = p.match(/[^\s]+/g);
+		var v = '';
+		for (var i = 0; i < t.length; i++) {
+			v += ':iValueContains(' + t[i] + ')';
+			if(i+1 !== t.length){
+                v += ',';
+            }
+		}
+		return jQuery(this).filter(v);
+    },
+	iValueSearch: function(p) {
+	   if (p.match(/.+/g) === null) return this;
+		return jQuery(this).filter(':iValueContains(' + p + ')');
+	}
+});
+
+//Special Searches
+jQuery.extend( jQuery.fn, {
+    wildcardSearch: function(p) {
+        var regex  = "^"+p.replace(/\?/g,'.{1}').replace(/\*/g,'.*?')+"$";
+        var re = new RegExp(regex, 'i');
+        var value = jQuery();
+        
+        jQuery(this).each(function(){
+            if(re.test(jQuery(this).text())){
+                value = jQuery(value).add(this); 
+            }
+        });
+        
+        return value;
+    },
+    regexSearch: function(regex) {
+        var value = jQuery();
+        jQuery(this).each(function(){
+            if(regex.test(jQuery(this).text())){
+                value = jQuery(value).add(this); 
+            }
+        });
+        
+        return value;
+    }
 });
